8af3d3b6a4f660a215f946a1fb545ba014794de0
System <none>
2017-06-30 12:39:31 +0000

    Checkpoint

---
+++ app.js
@@ -0,0 +1,18 @@
+var http = require("http"),
+    express = require("express"),
+    path = require('path'),
+    fs = require('fs'),
+    mustache = require("mustache");
+
+
+const app = express();
+/*var mustacheExpress = require('mustache-express');
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/public');
+/*app.get('/', function(req,res){
+    res.sendFile( '' )
+})*/
+
+app.listen(1010);
